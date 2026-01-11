be5aea50e226f6e461538c9f5a93b8a8689a4121
leodymann <hleodymann@gmail.com>
2026-01-11 20:04:16 -0300

    feat: Start implementng use_cases

---
+++ src/data/use_cases/user_finder_test.py
@@ -0,0 +1,7 @@
+from .user_finder import UserFinder
+from src .infra.db.repositories.users_repository import UsersRepository
+
+def test_find():
+    repo = UsersRepository()
+    user_finder = UserFinder(repo)
+    
