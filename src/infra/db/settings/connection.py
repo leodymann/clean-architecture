360e3a6afbeac5561ee5e6bd17dfd6c63956dd6f
leodymann <hleodymann@gmail.com>
2026-01-11 15:25:28 -0300

    config: Config local project + config database and connection configs

---
+++ src/infra/db/settings/connection.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+
+class DBConnectionHandler:
+
+    def __init__(self):
+        self.__connection_string = "{}://{}:{}@{}:{}/{}".format(
+            'mysql+pymysql',
+            'dev',
+            '123456',
+            '127.0.0.1',
+            '3306',
+            'clean_database'
+        )
+        self.__engine = self.__create_database_engine()
+
+    def __create_database_engine(self):
+        engine = create_engine(self.__connection_string)
+        return engine
+    
+    def get_engine(self):
+        return self.__engine
+    
---
+++ src/infra/db/settings/connection_test.py
@@ -0,0 +1,9 @@
+import pytest
+from .connection import DBConnectionHandler
+
+@pytest.mark.skip(reason="Sensive test") # teste sensível, pular nos testes
+def test_create_database_engine():
+    db_connection_handler = DBConnectionHandler()
+    engine = db_connection_handler.get_engine()
+
+    assert engine is not None
