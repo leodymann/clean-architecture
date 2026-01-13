887b119c4ec7fc024d839f39a5ff812c9fb9f252
leodymann <hleodymann@gmail.com>
2026-01-13 09:44:12 -0300

    feat: Implementing Validators

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -5,6 +5,9 @@ from src.main.adapters.request_adapter import request_adapter
 from src.main.composers.user_finder_composer import user_finder_composer
 from src.main.composers.user_register_composer import user_register_composer
 
+from src.validators.user_register_validator import user_register_validator
+from src.validators.user_finder_validator import user_finder_validator
+
 from src.errors.types.error_handle import handle_errors
 
 user_route_bp = Blueprint("user_routes", __name__)
@@ -15,6 +18,7 @@ def find_user():
     http_response = None
 
     try:
+        user_finder_validator(request)
         http_response = request_adapter(request, user_finder_composer())
     except Exception as exception:
         http_response = handle_errors(exception)
@@ -25,6 +29,7 @@ def register_user():
     http_response = None
 
     try:
+        user_register_validator(request)
         http_response = request_adapter(request, user_register_composer())
     except Exception as exception:
         http_response = handle_errors(exception)
